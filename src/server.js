02ddf191b79d43a42a2d7f63ae15c123c3536e3d
Muhaimin Abdillah <muhaiminabdillah85@gmail.com>
2022-09-16 21:06:14 +0700

    initial project

---
+++ src/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const Hapi = require('@hapi/hapi');
+
+const init = async ()=>{
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+
+  });
+  await server.start();
+  console.log(`Server running at ${server.info.uri}`);
+};
+
+init();
